0e3d841341230145acd55aa317230bc7e8a083d0
hehuas <754892110@qq.com>
2018-11-21 00:26:57 +0800

    header 95%

--- webpack.config.js
+++ webpack.config.js
@@ -45,52 +45,52 @@ module.exports = {
           { loader: 'postcss-loader' },
         ],
       },
-        {
-            test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
-            loader: 'url-loader',
-            options: {
-                limit: 10000,
-                minetype: 'application/font-woff',
-            },
+      {
+        test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          minetype: 'application/font-woff',
         },
-        {
-            test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
-            loader: 'url-loader',
-            options: {
-                limit: 10000,
-                minetype: 'application/font-woff',
-            },
+      },
+      {
+        test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          minetype: 'application/font-woff',
         },
-        {
-            test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
-            loader: 'url-loader',
-            options: {
-                limit: 10000,
-                minetype: 'application/octet-stream',
-            },
+      },
+      {
+        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          minetype: 'application/octet-stream',
         },
-        { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
-            loader: 'url-loader',
-            options: {
-                limit: 10000,
-                minetype: 'application/vnd.ms-fontobject',
-            },
+      },
+      { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          minetype: 'application/vnd.ms-fontobject',
         },
-        {
-            test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
-            loader: 'url-loader',
-            options: {
-                limit: 10000,
-                minetype: 'image/svg+xml',
-            },
+      },
+      {
+        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          minetype: 'image/svg+xml',
         },
-        {
-            test: /\.(png|jpg|jpeg|gif)(\?v=\d+\.\d+\.\d+)?$/i,
-            loader: 'url-loader',
-            options: {
-                limit: 10000,
-            },
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif)(\?v=\d+\.\d+\.\d+)?$/i,
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
         },
+      },
     ],
   },
   plugins: [
